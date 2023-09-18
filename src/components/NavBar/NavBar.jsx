27b7a45f09db40bcb20d0b571661e7ef16e6205e
unknown <muhammadjahidhasan18@gmail.com>
2023-09-18 20:47:49 +0600

    Therd commit

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,9 @@
+import { useState } from "react";
 import Link from "../Link/Link";
+import { AiOutlineMenu,AiOutlineCloseSquare } from 'react-icons/ai';
 const NavBar = () => {
+    const [open,setOpen] = useState(false)
+
     const routes = [
         { id: 1, path: "/", name: "Home" },
         { id: 2, path: "/about", name: "About" },
@@ -10,6 +14,14 @@ const NavBar = () => {
       
     return (
         <nav>
+           <div className="md:hidden text-2xl" onClick={() => setOpen(!open)} >
+            {
+                open === true ?
+                <AiOutlineCloseSquare></AiOutlineCloseSquare>
+                : <AiOutlineMenu ></AiOutlineMenu>
+            }
+           
+           </div>
           <ul className="md:flex ">
           {
                 routes.map(route => <Link key={route.id} route={route}></Link>)
